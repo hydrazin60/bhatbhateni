f0299fe79df2c3b57f3172ba91f74aae85a51b58
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-07 12:58:22 +0545

    basic frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+import { Button } from './components/ui/button'
+ 
+ export default function App() {
+   return (
+     <div>
+       <p className="text-3xl font-bold">App</p>
+       <Button>Button</Button>
+     </div>
+   )
+ }
+ 
